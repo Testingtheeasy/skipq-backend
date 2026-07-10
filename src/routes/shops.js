756482fa3494fa6dcce8c92bb9e2589fa010ecdb
Testingtheeasy <goodwingowthamraj333@gmail.com>
2026-07-10 19:49:22 +0530

    Add files via upload

--- src/routes/shops.js
+++ src/routes/shops.js
@@ -74,7 +74,7 @@ router.patch('/:id', async (req, res) => {
       'address','googleMapsUrl','ownerName','ownerPhone','ownerEmail',
       'openingTime','closingTime','isOpen','status','subscriptionPaid',
       'subscriptionExpiry','trialDays','ownerPassword',
-      'preOrderEnabled','preOrderPayment','preOrderDaysAhead',
+      'preOrderEnabled','preOrderPayment','preOrderDaysAhead','asapOnly','bannerImageUrl',
       'orderWindowStart','deliveryStart',
       'hasCustomHours','hasMultipleShifts','shiftSchedule','slotInterval',
     ];
